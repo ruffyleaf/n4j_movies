4170c552282c689962459a1bf6713664c46f5604
Jackson Maximilian Yap-Zhi-Chang  (NCS) <jacksonm.yap@ncs.com.sg>
2018-11-01 13:10:06 +0800

    created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='n4j_movies',
+    version='0.1',
+    description='Simple app to work with the neo4j movie database',
+    url='https://github.com/ruffyleaf/n4j_movies',
+    author='ruffyleaf',
+    author_email='ruffyleaf@hotmail.com',
+    packages=['n4j_movies'],
+    zip_safe=False
+)
